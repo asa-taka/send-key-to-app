1831b9f4d716cd5a7a9ab295bd67fb54ef923d58
lbcat <lightbulbcat@icloud.com>
2020-11-22 02:00:32 +0900

    First implementation

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # send-key-to-app
 
-A description of this package.
+A simple macOS CLI utility, `send-key-to-app` sends a key stroke to another application.
+It assumes to be used with key configuration utility softwares like
+[Karabiner-Elements](https://github.com/pqrs-org/Karabiner-Elements),
+to send a key stroke to a background application.
+
+## Usage
+
+```sh
+send-key-to-app Kindle 124 # let background Kindle to navigate next page
+send-key-to-app Kindle 123 # ...and page back
+```
+
+## Build and Install
+
+```sh
+git clone https://github.com/asa-taka/send-key-to-app
+cd send-key-to-app
+swift build -c release
+cp $(swift build -c release --show-bin-path)/send-key-to-app <YOUR_$PATH>
+```
--- Sources/send-key-to-app/main.swift
+++ Sources/send-key-to-app/main.swift
@@ -1,1 +1,39 @@
-print("Hello, world!")
+import Foundation
+import AppKit
+
+let args = CommandLine.arguments
+if args.count != 3 {
+  print("\(args[0]) [APP_NAME] [KEY_CODE]")
+  print("APP_NAME: an executable file name (not a path)")
+  print("KEY_CODE: a number such as 123(LEFT), 124(RIGHT), ...")
+  exit(1)
+}
+
+let appName = args[1]
+let keyCode = UInt16(args[2]) ?? 0
+
+let src = CGEventSource(stateID: CGEventSourceStateID.combinedSessionState)
+
+func sendKeyStroke(pid: pid_t, keyCode: UInt16) {
+  let keyUp = CGEvent(keyboardEventSource: src, virtualKey: keyCode, keyDown: true)
+  keyUp?.postToPid(pid)
+  usleep(1000) // required, but why...?
+  let keyDown = CGEvent(keyboardEventSource: src, virtualKey: keyCode, keyDown: false)
+  keyDown?.postToPid(pid)
+}
+
+func getPidByName(executableFileName: String) -> Optional<pid_t> {
+  let apps = NSWorkspace.shared.runningApplications
+  for a in apps {
+    if a.executableURL?.lastPathComponent == executableFileName {
+      return a.processIdentifier
+    }
+  }
+  return nil
+}
+
+if let pid = getPidByName(executableFileName: appName) {
+  sendKeyStroke(pid: pid, keyCode: keyCode)
+} else {
+  print("executable '\(appName)' not found in user processes")
+}
